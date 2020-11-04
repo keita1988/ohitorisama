7fe16271804a18519ea470c218ef2b5138a5de20
keita1988 <keita12may@gmail.com>
2020-11-04 18:01:06 +0900

    tweetモデルのアソシエーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,14 @@
 class Tweet < ApplicationRecord
+  has_many   :comments
+  belongs_to :user
+
+  with_options presence: true do
+    validates :coffee_price
+    validates :tasty
+    validates :relaxation
+    validates :under_ground
+    validates :store_name
+    validates :address
 end
 
 
